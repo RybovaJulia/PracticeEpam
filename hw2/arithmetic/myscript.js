0eb6eb794d4479d3723fd102a3c0f5aeb06fd82f
RybovaJulia <rybova.julia0908@gmail.com>
2017-03-28 23:27:49 +0300

    Homework

--- hw2/arithmetic/myscript.js
+++ hw2/arithmetic/myscript.js
@@ -1,21 +1,39 @@
-/*вывести число минут и сеукунд до конца сегодняшнего дня*/
-var tomorrow = new Date().setHours(24, 0, 0);
+var n = parseInt(prompt("Введите n количество элементов массива:"));
+var s = parseInt(prompt("Введите значение s"));
+var p = parseInt(prompt("Введите значение p"));
+var per,avr=0;
+var arr = new Array(n);
+per=s*p/100;
+var max=s+per;
+var min=s-per;
+var maxN;
+var maxIndex;
+var sum = 0;
 
-(function foo() {
-  var now = new Date();
+if(isFinite(n) && isFinite(p) && isFinite(s)){
+	for(var i = 0; i < arr.length; i++){
+	arr[i]=Math.round((Math.random()*(max-min)+min)*100)/100;	
+}
+console.log(arr);
+for(var j=0; j<n; j++){
+	maxN = arr[0];
+	maxIndex = 0;
+	for(var  i =0; i < n-j; i++){
+		if(arr[i] > maxN){
+			maxN = arr[i];
+			maxIndex = i;
+			
+		}
 
-  // Сколько секунд осталось до завтра.
-  var seconds = parseInt(tomorrow - now) / 1000;
-
-  // 3600 - кол-во секунд в часе. Сколько часов осталось до завтра
-  var hh = parseInt( seconds / 3600 );
-  var mm = parseInt( (seconds % 3600) / 60 );
-  var ss = parseInt( (seconds % 3600) % 60 );
-  
- 
-  
-  alert("До конца дня осталось: "+hh+" часa "+mm+" минут "+ss+ " секунд");
-
-
-
-})();
+	}
+	arr[maxIndex] = arr[n-j-1];
+	arr[n-j-1] = maxN;
+	sum += maxN;
+}
+console.log(arr);
+console.log("Сумма: " + sum);
+console.log("Среднее арифметическое равно: " + sum/n);
+}
+else{
+	alert("Повторите попытку")
+}
