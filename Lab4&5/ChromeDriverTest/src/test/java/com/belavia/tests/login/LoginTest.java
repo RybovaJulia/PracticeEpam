8b8c344b9150aa355a4ec3351abd47240b70ab08
RybovaJulia <rybova.julia0908@gmail.com>
2017-12-13 12:11:57 +0300

    Update LoginTest.java

--- Lab4&5/ChromeDriverTest/src/test/java/com/belavia/tests/login/LoginTest.java
+++ Lab4&5/ChromeDriverTest/src/test/java/com/belavia/tests/login/LoginTest.java
@@ -17,5 +17,41 @@ public class LoginTest extends FunctionalTest {
     private static final String LOGIN_FAILED_EXPECTED_MASSAGE = "Invalid email or password.";
     private static final String LOGIN_EXPECTED_MASSAGE = "Sign out";
 
+ @Test(priority = 0)
+    public void loginFailed(){
+        driver.get(PATH);
+
+        LoginPage loginPage = new LoginPage(driver);
+        assertTrue(loginPage.isInitialized());
+
+        loginPage.enterName(USER_UNVALID_EMAIL, USER_VALID_PASSWORD);
+
+        loginPage.submit();
+        assertTrue(loginPage.isNotifictionEnable());
+
+        assertEquals(LOGINFAILED_EXPECTED_MASSAGE, loginPage.getNotificationMassage());
+    }
+
+    @Test (priority = 1)
+    public void login(){
+        driver.get(PATH);
+
+        LoginPage loginPage = new LoginPage(driver);
+        assertTrue(loginPage.isInitialized());
+
+        loginPage.enterName(USER_VALID_EMAIL, USER_VALID_PASSWORD);
+
+        HomePage homePage = loginPage.submit();
+        assertTrue(homePage.isPageOpened());
+
+        assertEquals(LOGIN_EXPECTED_MASSAGE, homePage.confirmationTitle());
+    }
+    
+    @Test(priority = 2)
+    public void screeningPage(){
+        ScreeningPage screeningPage = new ScreeningPage(driver);
+        assertTrue(screeningPage.isClientsListExist());
+    }
+}
 
    
