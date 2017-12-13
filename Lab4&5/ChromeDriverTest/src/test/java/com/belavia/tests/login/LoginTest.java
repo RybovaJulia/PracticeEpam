8cc8ae9e4869b1f4ab12de4713b857fe7aa4bd32
RybovaJulia <rybova.julia0908@gmail.com>
2017-12-13 12:06:01 +0300

    Update LoginTest.java

--- Lab4&5/ChromeDriverTest/src/test/java/com/belavia/tests/login/LoginTest.java
+++ Lab4&5/ChromeDriverTest/src/test/java/com/belavia/tests/login/LoginTest.java
@@ -18,34 +18,4 @@ public class LoginTest extends FunctionalTest {
     private static final String LOGIN_EXPECTED_MASSAGE = "Sign out";
 
 
-    @Test(priority = 0)
-    public void loginFailed(){
-        driver.get(PATH);
-
-        LoginPage loginPage = new LoginPage(driver);
-        assertTrue(loginPage.isInitialized());
-
-        loginPage.enterName(USER_UNVALID_EMAIL, USER_VALID_PASSWORD);
-
-        loginPage.submit();
-        assertTrue(loginPage.isNotifictionEnable());
-
-        assertEquals(LOGINFAILED_EXPECTED_MASSAGE, loginPage.getNotificationMassage());
-    }
-
-    @Test (priority = 1)
-    public void login(){
-        driver.get(PATH);
-
-        LoginPage loginPage = new LoginPage(driver);
-        assertTrue(loginPage.isInitialized());
-
-        loginPage.enterName(USER_VALID_EMAIL, USER_VALID_PASSWORD);
-
-        HomePage homePage = loginPage.submit();
-        assertTrue(homePage.isPageOpened());
-
-        assertEquals(LOGIN_EXPECTED_MASSAGE, homePage.confirmationTitle());
-    }
-
-}
+   
