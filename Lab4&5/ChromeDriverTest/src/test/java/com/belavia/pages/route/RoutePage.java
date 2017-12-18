c2db23256be350c18d83d69310f50b41df5cae7e
RybovaJulia <rybova.julia0908@gmail.com>
2017-12-18 20:52:17 +0300

    Update RoutePage.java

--- Lab4&5/ChromeDriverTest/src/test/java/com/belavia/pages/route/RoutePage.java
+++ Lab4&5/ChromeDriverTest/src/test/java/com/belavia/pages/route/RoutePage.java
@@ -7,84 +7,39 @@ import org.openqa.selenium.support.FindBy;
 
 
 public class RoutePage extends BasePage {
-    @FindBy(id = "originStation")
-    WebElement originStation;
-
-    @FindBy(id = "destinationStation")
-    WebElement destinationStation;
-
-    @FindBy(id = "submitButton")
-    WebElement submitButton;
-
-    @FindBy(id = "journey-type-return")
-    WebElement returnRatio;
-
-    @FindBy(id = "journey-type-single")
-    WebElement oneWayRatio;
-
-    @FindBy(xpath = "//*[@id=\"extendedSearchForm\"]/div[3]/div[2]/div/div[1]/button[2]")
-    WebElement nextDay;
-
-    @FindBy(xpath = "//*[@id=\"extendedSearchForm\"]/div[3]/div[1]/div/div[1]/button[1]")
-    WebElement today;
-
-    @FindBy(xpath = "//*[@id=\"tickets\"]/div/div[2]/table/thead/tr[1]/th[2]/div/div[1]")
-    WebElement returnRoute;
-
-    @FindBy(id = "swap")
-    WebElement swapButton;
-
-    @FindBy(xpath = "//*[@id=\"timetable\"]/h1")
-    WebElement swap;
     
-    public RoutePage(WebDriver driver) {
-        super(driver);
+    private final String BASE_URL = "https://belavia.by/time-table/";
+
+    private final Logger logger   = LogManager.getRootLogger();
+
+    public TimetablePage(WebDriver webDriver) {
+        super(webDriver);
+        PageFactory.initElements(this.webDriver, this);
     }
 
-    public boolean isInitialized() {
-        return originStation.isDisplayed();
+    @FindBy(id = "origin_Combobox")
+    private WebElement inputOrigin;
+
+    @FindBy(id = "destination_Combobox")
+    private WebElement inputDestination;
+
+    @FindBy(id = "getTimeTable")
+    private WebElement getTimeTableBtn;
+
+    @Override
+    public void open() {
+        webDriver.navigate().to(BASE_URL);
+        logger.info("Time Table page opened");
     }
 
-    public void enterRoute(String originStation, String destinationStation) {
-        this.originStation.clear();
-        this.originStation.sendKeys(originStation);
-
-        this.destinationStation.clear();
-        this.destinationStation.sendKeys(destinationStation);
+    public void searchTimetable(String origin, String destination){
+        inputOrigin.sendKeys(origin);
+        inputDestination.sendKeys(destination);
+        searchBtn.click();
+        logger.info("search time table performed");
     }
 
-    public void submit() {
-        submitButton.click();
-    }
-
-    public void returnTicket() {
-        returnRatio.click();
-        nextDay.click();
-    }
-
-    public void setOneWayRatioTicket() {
-        today();
-        oneWayRatio.click();
-    }
-
-    public void today(){
-        today.click();
-    }
-
-    public void swap(){
-        today();
-        swapButton.click();
-    }
-
-    public String swapTitle() {
-        return swap.getText();
-    }
-
-    public String confirmationOneWayTitle() {
-        return message.getText();
-    }
-
-    public  String confirmationReturnTitle(){
-        return returnRoute.getText();
-    }
 }
+  
+
+   
