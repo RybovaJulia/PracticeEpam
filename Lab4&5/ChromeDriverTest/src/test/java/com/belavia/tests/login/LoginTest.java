485634c4efa483da8e5cd7df0b940f4023018799
RybovaJulia <rybova.julia0908@gmail.com>
2017-12-18 21:29:37 +0300

    Update LoginTest.java

--- Lab4&5/ChromeDriverTest/src/test/java/com/belavia/tests/login/LoginTest.java
+++ Lab4&5/ChromeDriverTest/src/test/java/com/belavia/tests/login/LoginTest.java
@@ -9,13 +9,13 @@ import static com.thoughtworks.selenium.SeleneseTestBase.assertEquals;
 import static com.thoughtworks.selenium.SeleneseTestBase.assertTrue;
 
 public class HomePageTest extends FunctionalTest {
-
-    private static final String  USER_ID = "10011526537623843456678";
-    private static final String PASSWORD = "123456789";
-    public static final String EXPECTED_NAME = "JULIA RYBOVA";
-        
+    
+    private static final String  USER_ID = "100114162345679";
+    private static final String PASSWORD = "12345678912345";
+    private static final String PASSPORT_ID = "MC2099742";
     private Steps steps;
-        
+    private static final String COUNTRY = "RUSSIA";
+
     @BeforeMethod(description = "Init browser")
     public void setUp()
     {
@@ -23,23 +23,23 @@ public class HomePageTest extends FunctionalTest {
         steps.initBrowser();
     }
 
-    @Test
-    public void accessToMainPageTest() {
-        Assert.assertTrue(steps.accessToMainPage());
-    }
-
-    @Test
-    public void menuTest() {
-        Assert.assertTrue(steps.checkMenu());
-    }
-
-    @Test(description = "auth test")
-    public void authTest() {
-
+        @Test(description = "change passport id")
+    public void changePassportDataTest() {
         String username = steps.login(USER_ID,PASSWORD);
-        Assert.assertEquals(username, EXPECTED_NAME);
+        Assert.assertEquals(steps.changePassportId(PASSPORT_ID),PASSPORT_ID);
     }
 
+    @Test(description = "change country")
+    public void changeCountry() {
+        steps.login(USER_ID,PASSWORD);
+        Assert.assertEquals(steps.changeCountry(COUNTRY),COUNTRY);
+    }
+
+    @Test(description = "change place of employment")
+    public void changePlaceOfEmployment() {
+        steps.login(USER_ID,PASSWORD);
+        Assert.assertEquals(steps.changePlaceOfEmpl(PLACE_OF_EMPLOYMENT),PLACE_OF_EMPLOYMENT);
+    }
 
 
     @AfterMethod(description = "Stop Browser")
@@ -47,37 +47,7 @@ public class HomePageTest extends FunctionalTest {
     {
         steps.closeDriver();
     }
-}
 
- @Test(priority = 0)
-    public void loginFailed(){
-        driver.get(PATH);
-
-        LoginPage loginPage = new LoginPage(driver);
-        assertTrue(loginPage.isInitialized());
-
-        loginPage.enterName(USER_UNVALID_EMAIL, USER_VALID_PASSWORD);
-
-        loginPage.submit();
-        assertTrue(loginPage.isNotifictionEnable());
-
-        assertEquals(LOGINFAILED_EXPECTED_MASSAGE, loginPage.getNotificationMassage());
-    }
-
-    @Test (priority = 1)
-    public void login(){
-        driver.get(PATH);
-
-        LoginPage loginPage = new LoginPage(driver);
-        assertTrue(loginPage.isInitialized());
-
-        loginPage.enterName(USER_VALID_EMAIL, USER_VALID_PASSWORD);
-
-        HomePage homePage = loginPage.submit();
-        assertTrue(homePage.isPageOpened());
-
-        assertEquals(LOGIN_EXPECTED_MASSAGE, homePage.confirmationTitle());
-    }
 }
 
    
