f6a3fcc073f6ff075468832b94f36ae92f8205e6
RybovaJulia <rybova.julia0908@gmail.com>
2017-12-18 20:43:18 +0300

    Update HomePage.java

--- Lab4&5/ChromeDriverTest/src/test/java/com/belavia/pages/home/HomePage.java
+++ Lab4&5/ChromeDriverTest/src/test/java/com/belavia/pages/home/HomePage.java
@@ -6,23 +6,61 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class HomePage extends BasePage {
+    
+   private final Logger logger   = LogManager.getRootLogger();
 
-    public HomePage(WebDriver driver)
-    {
-        super(driver);
+   private final String BASE_URL = "https://belavia.by/";
+
+   
+     public HomePage(WebDriver webDriver) {
+        super(webDriver);
+        PageFactory.initElements(this.webDriver, this);
     }
-    @FindBy(className = "title-text")
-    private WebElement heading;
+    
+    @FindBy(id = "select-member")
+    private WebElement signInBtn;
 
-    public boolean isPageOpened() {
+    @FindBy(id = "MemberId")
+    private WebElement inputLogin;
 
-        return heading.isDisplayed();
+    @FindBy(id = "Password")
+    private WebElement inputPassword;
+
+    @FindBy(className = "btn-b2-login")
+    private WebElement signInSubmitBtn;
+    
+    
+    
+    @FindBy(id="select-lang")
+    WebElement changeLanguageBtn;
+
+    @FindBy(xpath = "//a[@data-iso='EN']")
+    WebElement languageToChoose;
+
+
+    @Override
+    public void open() {
+        webDriver.navigate().to(BASE_URL);
+        logger.info("Main page opened");
     }
 
-    public String confirmationTitle(){
-
-        return heading.getText();
+    public void login(String username, String password) {
+        signInBtn.click();
+        inputLogin.sendKeys(username);
+        inputPassword.sendKeys(password);
+        signInSubmitBtn.submit();
+        logger.info("Sign In performed");
     }
 
+    public void changeLanguage(){
+        changeLangeageBtn.click();
+        logger.info("click on dropdown to change language");
+        languageToChoose.click();
+        Driver.getDriver().manage().timeouts().setScriptTimeout(30, TimeUnit.SECONDS);
+        logger.info("choose language");
+    }
 }
+    
+    
+   
 
