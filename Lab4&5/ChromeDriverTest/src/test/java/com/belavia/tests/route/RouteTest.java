5cd02012c94c115eeb6a72d633b80f3d67abef3e
RybovaJulia <rybova.julia0908@gmail.com>
2017-12-18 21:38:59 +0300

    Update RouteTest.java

--- Lab4&5/ChromeDriverTest/src/test/java/com/belavia/tests/route/RouteTest.java
+++ Lab4&5/ChromeDriverTest/src/test/java/com/belavia/tests/route/RouteTest.java
@@ -13,52 +13,37 @@ import static com.thoughtworks.selenium.SeleneseTestBase.assertTrue;
 
 public class RouteTest extends DriverTest {
 
-    private static final String PATH = "https://www.belavia.com/";
-    private static final String ORIGIN_STATION = "Minsk";
-    private static final String DESTINATION_STATION = "Moscow";
-    private static final String TODAY_EXPECTED_DATE = new SimpleDateFormat("E dd'th' MMM yyyy", Locale.US).format(Calendar.getInstance().getTime());
-    private static final String RETURN_ROUTE = "RETURN";
-    private static final String EXPECTED_SWAP = "Moscow (Any) to Minsk  (Any)";
+   private static final String FLY_FROM = "Moscow";
+    private static final String FLY_TO = "Minsk";
+    private Steps steps;
 
-    @Test (priority = 0)
-    public void returnRouteMinskMoscowToday(){
-        driver.get(PATH);
-
-        RoutePage routePage = new RoutePage(driver);
-        assertTrue(routePage.isInitialized());
-
-        routePage.enterRoute(ORIGIN_STATION, DESTINATION_STATION);
-        routePage.returnTicket();
-        routePage.submit();
-
-        assertEquals(RETURN_ROUTE, routePage.confirmationReturnTitle());
-        
-        @Test (priority = 1)
-    public void oneWayRouteMinskMoscowToday(){
-        driver.get(PATH);
-
-        RoutePage routePage = new RoutePage(driver);
-        assertTrue(routePage.isInitialized());
-
-        routePage.enterRoute(ORIGIN_STATION, DESTINATION_STATION);
-        routePage.setOneWayRatioTicket();
-        routePage.submit();
-
-        assertEquals(TODAY_EXPECTED_DATE, routePage.confirmationOneWayTitle());
+    @BeforeMethod(description = "Init browser")
+    public void setUp()
+    {
+        steps = new Steps();
+        steps.initBrowser();
     }
 
-    @Test (priority = 2)
-    public void routeMinskMoscowSwap(){
-        driver.get(PATH);
 
-        RoutePage routePage = new RoutePage(driver);
-        assertTrue(routePage.isInitialized());
+    @Test(description = "timetable access test")
+    public void timetableAccessTest() {
+        Assert.assertTrue(steps.checkAccessToTte());
+    }
 
-        routePage.enterRoute(ORIGIN_STATION, DESTINATION_STATION);
+    @Test(description = "booking flights test")
+    public void bookingOfFlightsTest() {
+        Assert.assertTrue(steps.bookFlights(FLY_FROM,FLY_TO));
+    }
 
-        routePage.swap();
-        routePage.submit();
+    @Test(description = "booking status checking test")
+    public void bookingStatusCheckingTestt() {
+        Assert.assertTrue(steps.canCheckBooking());
+    }
 
-        assertEquals(EXPECTED_SWAP, routePage.swapTitle());
+
+    @AfterMethod(description = "Stop Browser")
+    public void stopBrowser()
+    {
+        steps.closeDriver();
     }
 }
