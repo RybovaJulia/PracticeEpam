7ec0599e4771bcf4fc22b539ecbae4a879f0b280
RybovaJulia <rybova.julia0908@gmail.com>
2017-12-18 21:33:38 +0300

    Update HomePage.java

--- Lab4&5/ChromeDriverTest/src/test/java/com/belavia/pages/home/HomePage.java
+++ Lab4&5/ChromeDriverTest/src/test/java/com/belavia/pages/home/HomePage.java
@@ -6,57 +6,84 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class HomePage extends BasePage {
-    
-   private final Logger logger   = LogManager.getRootLogger();
+    private static final Logger logger = LogManager.getRootLogger();
+    public final String BASE_URL = "https://belavia.by/";
 
-   private final String BASE_URL = "https://belavia.by/";
-   
-     public HomePage(WebDriver webDriver) {
-        super(webDriver);
-        PageFactory.initElements(this.webDriver, this);
+    @FindBy(css = ".logo > a:nth-child(1) > img:nth-child(1)")
+    private WebElement logo;
+    
+    @FindBy(xpath = "//*[@id=\"select-main-menu\"]")
+    private WebElement menu;
+    
+    @FindBy(css = "menu-content a")
+    private WebElement menuItem;
+    
+    @FindBy(css = "№select-member")
+    private WebElement usernameElement;
+    
+    @FindBy(xpath = "//li[2]/a/span[2]")
+    private WebElement loginDropdownShowBtn;
+    
+    @FindBy(xpath = "//*[@id=\"MemberId\"]")
+    private WebElement loginInp;
+    
+    @FindBy(xpath = "//*[@id=\"Password\"]")
+    private WebElement passwordInp;
+    
+    @FindBy(css = ".form-horizontal .btn-b2-login")
+    private WebElement submitLoginBtn;
+
+    public MainPage(WebDriver driver) {
+        super(driver);
+        PageFactory.initElements(driver, this);
     }
-    
-    @FindBy(id = "select-member")
-    private WebElement signInBtn;
-
-    @FindBy(id = "MemberId")
-    private WebElement inputLogin;
-
-    @FindBy(id = "Password")
-    private WebElement inputPassword;
-
-    @FindBy(className = "btn-b2-login")
-    private WebElement signInSubmitBtn;
-    
-    @FindBy(id="select-lang")
-    WebElement changeLanguageBtn;
-
-    @FindBy(xpath = "//a[@data-iso='EN']")
-    WebElement languageToChoose;
 
     @Override
-    public void open() {
-        webDriver.navigate().to(BASE_URL);
-        logger.info("Main page opened");
+    public void openPage() {
+        logger.info("main page opened");
+        driver.navigate().to(BASE_URL);
     }
 
-    public void login(String username, String password) {
-        signInBtn.click();
-        inputLogin.sendKeys(username);
-        inputPassword.sendKeys(password);
-        signInSubmitBtn.submit();
-        logger.info("Sign In performed");
+
+    public boolean isLogoAvailable() {
+
+        return logo.isDisplayed();
     }
 
-    public void changeLanguage(){
-        changeLangeageBtn.click();
-        logger.info("click on dropdown to change language");
-        languageToChoose.click();
-        Driver.getDriver().manage().timeouts().setScriptTimeout(30, TimeUnit.SECONDS);
-        logger.info("choose language");
+    public boolean isMenuDisplayed() {
+
+        Actions actions = new Actions(driver);
+        actions
+                .click(menu)
+                .pause(1000)
+                .moveToElement(menuItem)
+                .build()
+                .perform();
+
+        return menuItem.isDisplayed();
+    }
+
+    public void login(String userId, String password) {
+        Actions actions = new Actions(driver);
+
+        actions
+                .click(loginDropdownShowBtn)
+                .sendKeys(loginInp,userId)
+                .sendKeys(passwordInp, password)
+                .moveToElement(submitLoginBtn)
+                .click(submitLoginBtn)
+                .build()
+                .perform();
+
+        System.out.println("test");
+
+    }
+
+
+    public String getUsername() {
+        logger.info("getting username");
+
+        return usernameElement.getText();
     }
 }
-    
-    
-   
-
+  
